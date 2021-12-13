ee54c1e7d8f267e17742e541cc1759fdfd622014
Rune Vestvik <rune@lightspeedgraphics.co.nz>
2021-12-13 22:27:41 +0100

    read port from env

--- scripts/server.js
+++ scripts/server.js
@@ -2,15 +2,13 @@
 const colors = require('colors/safe');
 const gaze = require('gaze');
 const StaticServer = require('static-server');
-
 const buildCSS = require('./build_css.js');
-
 
 gaze(['css/**/*.css'], (err, watcher) => {
   watcher.on('all', () => buildCSS());
 });
 
-const server = new StaticServer({ rootPath: process.cwd(), port: 8080, followSymlink: true });
+const server = new StaticServer({ rootPath: process.cwd(), port: process.env.PORT || 8080, followSymlink: true });
 server.start(() => {
   console.log(colors.yellow(`Listening on ${server.port}`));
 });
